fac0ec962805c6be5d5dc62ffcaea0eb5168d0e2
machaoma <88501614@qq.com>
2020-11-26 19:30:09 +0800

    Initial commit

---
+++ src/main/java/com/mark/tools/CovertToFile.java
@@ -0,0 +1,31 @@
+package com.mark.tools;
+
+/**
+ * 将bean写入文件
+ */
+public class CovertToFile {
+
+
+    /**
+     * 构建实例
+     * 构建上下文
+     *
+     * @return
+     */
+    public CovertToFile build() {
+        CovertToFile covertToFile = new CovertToFile();
+        //数据初始化
+        return covertToFile;
+    }
+
+
+    public void save() {
+
+    }
+
+
+    public void saveBatch() {
+
+    }
+
+}
